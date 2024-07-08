953057adaaff23fca46b48bd617f4564f9e9b91b
i21higasi <i21higasi@tokuyama.kosen-ac.jp>
2024-07-08 22:02:14 +0900

    課題10

--- mysystem.c
+++ mysystem.c
@@ -10,15 +10,72 @@
 
 // system関数のクローン
 int mysystem(char *command) {
-  int status = 0;
+  int status;
+  int pid ;
 
-  // ここにプログラムを書く
+  if((pid = fork()) < 0){ //fork失敗
+  return -1;
+  }
+
+  if(command == NULL){ //NULLポインタの場合
+    return 1;
+  }
+
+  if(pid != 0){
+    //親プロセス
+    while(wait(&status) != pid);
+  }else{
+    //子プロセス
+    execl("/bin/sh","sh","-c",command,NULL);
+    perror("/bin/sh");
+    exit(127);
+  }
 
   return status;
 }
 
 /* 実行例
-
-ここに実行例を書く
-
+% make
+cc -Wall -std=c99 -o mysysmain mysysmain.c  mysystem.c
+% ./mysysmain
+使い方 : ./mysysmain コマンド文字列
+% ./mysysmain pwd    
+mysystem:
+/Users/higashiyuinin/OneDrive - 独立行政法人 国立高等専門学校機構/Syspro2/kadai10-i21higasi
+retval = 00000000
+system:
+/Users/higashiyuinin/OneDrive - 独立行政法人 国立高等専門学校機構/Syspro2/kadai10-i21higasi
+retval = 00000000
+% ./mysysmain ls
+mysystem:
+Makefile        README.md       README.pdf      mysysmain       mysysmain.c     mysystem.c      mysystem.h
+retval = 00000000
+system:
+Makefile        README.md       README.pdf      mysysmain       mysysmain.c     mysystem.c      mysystem.h
+retval = 00000000
+% ./mysysmain ls -l
+使い方 : ./mysysmain コマンド文字列
+% ./mysysmain "ls -l"
+mysystem:
+total 616
+-rw-r--r--  1 higashiyuinin  staff     143  7  4 09:54 Makefile
+-rw-r--r--  1 higashiyuinin  staff    2795  7  4 09:54 README.md
+-rw-r--r--  1 higashiyuinin  staff  238232  7  4 09:54 README.pdf
+-rwxr-xr-x  1 higashiyuinin  staff   50358  7  8 22:00 mysysmain
+-rw-r--r--  1 higashiyuinin  staff     925  7  4 09:54 mysysmain.c
+-rw-r--r--  1 higashiyuinin  staff     734  7  8 22:00 mysystem.c
+-rw-r--r--  1 higashiyuinin  staff      90  7  4 09:54 mysystem.h
+retval = 00000000
+system:
+total 616
+-rw-r--r--  1 higashiyuinin  staff     143  7  4 09:54 Makefile
+-rw-r--r--  1 higashiyuinin  staff    2795  7  4 09:54 README.md
+-rw-r--r--  1 higashiyuinin  staff  238232  7  4 09:54 README.pdf
+-rwxr-xr-x  1 higashiyuinin  staff   50358  7  8 22:00 mysysmain
+-rw-r--r--  1 higashiyuinin  staff     925  7  4 09:54 mysysmain.c
+-rw-r--r--  1 higashiyuinin  staff     734  7  8 22:00 mysystem.c
+-rw-r--r--  1 higashiyuinin  staff      90  7  4 09:54 mysystem.h
+retval = 00000000
 */
+
+
